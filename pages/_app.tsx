3ad2d1a3fcd8434c6e8a4215395cf6b19b3856f8
Lucas Carneiro <lucascarneiro.profissional@gmail.com>
2022-03-14 20:27:21 -0300

    Version 1.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { DeviceContext } from "_config/context/device";
 
 // Types
 import { Devices } from "_config/device/types";
-import { Loader } from "templates/Loader/Loader";
+// import { Loader } from "templates/Loader/Loader";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     const [device, setDevice] = useState<Devices>(Devices.undefined);
